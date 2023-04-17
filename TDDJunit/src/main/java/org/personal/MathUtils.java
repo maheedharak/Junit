77562c44a2a25b3b9b3c3e3123c035aea1a92d8b
maheedharak <115575926+maheedharak@users.noreply.github.com>
2023-04-17 12:45:36 +0200

    TDD example

---
+++ TDDJunit/src/main/java/org/personal/MathUtils.java
@@ -0,0 +1,11 @@
+package org.personal;
+
+public class MathUtils {
+    public int add(int a,int b){
+        return a + b;
+    }
+    public double calculateCircleArea(double radius){
+        return Math.PI * radius * radius;
+    }
+}
+
---
+++ TDDJunit/src/test/java/org/personal/MathUtilsTest.java
@@ -0,0 +1,21 @@
+package org.personal;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+    @Test
+    public void addTest(){
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1,1);
+        assertEquals(expected,actual,"The method to test adding two numbers");
+    }
+    @Test
+    public void calculateCircleAreaTest(){
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793,mathUtils.calculateCircleArea(10));
+    }
+
+}
